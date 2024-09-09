05b1f1c3b42f99967cd06138dc4792a25411fd6f
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-09-09 16:54:29 +0300

    Index

--- Company.G03.DAL/Models/Department.cs
+++ Company.G03.DAL/Models/Department.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,14 @@ namespace Company.G03.DAL.Models
     public class Department
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name Is Required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Code Is Required")]
         public string Code { get; set; }
+
+        [DisplayName("Date Of Creation")]
         public DateTime DateOfCreation { get; set; }
     }
 }
