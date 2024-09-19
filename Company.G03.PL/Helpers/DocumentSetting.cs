d4de61ddc541d626adce7337c3b2507f8f8b2e41
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-09-19 14:45:19 +0300

    Document Setting - Delete

--- Company.G03.PL/Helpers/DocumentSetting.cs
+++ Company.G03.PL/Helpers/DocumentSetting.cs
@@ -10,11 +10,19 @@
 
             string filePath = Path.Combine(folderPath, fileName);
 
-            var fileStream = new FileStream(filePath,FileMode.Create);
+            using var fileStream = new FileStream(filePath,FileMode.Create);
 
             file.CopyTo(fileStream);
 
             return fileName;
+        }
+
+        public static void DeleteFile(string fileName, string folderName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(),@"wwwroot\files",folderName,fileName);
+
+            if(File.Exists(filePath))
+                File.Delete(filePath);
         }
     }
 }
