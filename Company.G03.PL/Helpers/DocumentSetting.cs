6ab7573044997ea6360161e8a9e54635c7647466
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-09-19 14:38:46 +0300

    Document Setting - Upload

---
+++ Company.G03.PL/Helpers/DocumentSetting.cs
@@ -0,0 +1,20 @@
+﻿namespace Company.G03.PL.Helpers
+{
+    public static class DocumentSetting
+    {
+        public static string UploadFile(IFormFile file, string folderName)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\files",folderName);
+
+            string fileName = $"{Guid.NewGuid()}{file.FileName}";
+
+            string filePath = Path.Combine(folderPath, fileName);
+
+            var fileStream = new FileStream(filePath,FileMode.Create);
+
+            file.CopyTo(fileStream);
+
+            return fileName;
+        }
+    }
+}
