792aeff35246ff95b3fa073113c72256d98dd343
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-09-19 21:05:45 +0300

    Microsoft Identity Package

--- Company.G03.DAL/Data/Contexts/AppDbContext.cs
+++ Company.G03.DAL/Data/Contexts/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Company.G03.DAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,13 +10,14 @@ using System.Threading.Tasks;
 
 namespace Company.G03.DAL.Data.Contexts
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
         }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
