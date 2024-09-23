4ac5e02c67b036c6c9a5d5ee31424aef3208f130
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-09-23 17:40:33 +0300

    Account Controller - SignIn

--- Company.G03.PL/Controllers/AccountController.cs
+++ Company.G03.PL/Controllers/AccountController.cs
@@ -5,28 +5,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G03.PL.Controllers
 {
-    public class AccountController : Controller
-    {
+	public class AccountController : Controller
+	{
 		private readonly UserManager<AppUser> _UserManager;
 
 		public AccountController(UserManager<AppUser> userManager)
-        {
+		{
 			_UserManager = userManager;
 		}
 
-
+		#region SignUp
 		[HttpGet]
-        public IActionResult SignUp()
-        {
-            return View();
-        }
+		public IActionResult SignUp()
+		{
+			return View();
+		}
 
 		[HttpPost]
 		public async Task<IActionResult> SignUp(SignUpViewModel model)
 		{
-            if(ModelState.IsValid)
-            {
-				var User = await  _UserManager.FindByNameAsync(model.UserName);
+			if (ModelState.IsValid)
+			{
+				var User = await _UserManager.FindByNameAsync(model.UserName);
 
 				if (User is null)
 				{
@@ -63,8 +63,49 @@ namespace Company.G03.PL.Controllers
 				}
 
 				ModelState.AddModelError(string.Empty, "User Name Is Already Exist !");
-            }
+			}
 			return View(model);
 		}
+
+		#endregion
+
+		#region SignIn
+		[HttpGet]
+		public IActionResult SignIn()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SignIn(SignInViewModel model)
+		{
+            if (ModelState.IsValid)
+            {
+				try
+				{
+					var user = await _UserManager.FindByEmailAsync(model.Email);
+					if (user is not null)
+					{
+						var flag = await _UserManager.CheckPasswordAsync(user, model.Password);
+
+						if (flag)
+						{
+							return RedirectToAction("Index", "Home");
+						}
+					}
+					ModelState.AddModelError(string.Empty, "Login Is Invalid !!");
+
+				}
+				catch (Exception ex)
+				{
+					ModelState.AddModelError(string.Empty, ex.Message);
+				}
+
+            }
+            return View();
+		}
+		#endregion
+
+
 	}
 }
---
+++ Company.G03.PL/Models/SignInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.G03.PL.Models
+{
+	public class SignInViewModel
+	{
+		[EmailAddress(ErrorMessage = "Eamil Is Invalid")]
+		[Required(ErrorMessage = "Email Is Required !")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Password Is Required !")]
+		[DataType(DataType.Password)]
+		[MinLength(5, ErrorMessage = "Password Min Lenght Is 5")]
+		public string Password { get; set; }
+
+		public bool RememberMe { get; set; }
+	}
+}
